60d7dea09b45a40d92532b0e333163b73db42af8
Eduard Castro <eduard.cas07@gmail.com>
2019-05-21 13:38:43 -0500

    Add forms

---
+++ forms/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const form = document.getElementById('user-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // const user = form.elements['user'];
+});
