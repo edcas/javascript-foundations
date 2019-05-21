e335daaf49d0a3ef6ee0bddf43876509f2a518f1
Eduard Castro <eduard.cas07@gmail.com>
2019-05-21 13:38:07 -0500

    Add BOM and DOM

---
+++ bom-and-dom/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let element = document.getElementById('first');
+element.textContent = 'new text here';
+element.setAttribute('name', 'nameValue');
+element.classList.add('myClassName');
+element.style.color = 'blue';
