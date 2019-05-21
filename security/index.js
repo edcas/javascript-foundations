54f48c16dcfbae9d04c2bd6dac8f66564d54a60a
Eduard Castro <eduard.cas07@gmail.com>
2019-05-21 13:38:54 -0500

    Add security

---
+++ security/index.js
@@ -0,0 +1,17 @@
+
+// eval()
+// La función eval() evalúa un código JavaScript representado como una cadena de caracteres (string), sin referenciar a un objeto en particular
+
+
+// Security  ofuscar el codigo javascript - https://javascriptobfuscator.com/
+
+
+// Prevent Man in the middle attacks
+// use SSL
+// Use HTTP Header: Strict-Transport-Security
+// Use cookie attribute: Secure and HttpOnly
+
+// Addressing Cross site Scripting XSS
+// CSP: Content Security Policy -> Use HTTP Header: Content-Security-Policy
+// CORS: Cross Origin Resource Sharing -> Use HTTP Header: Access-Control-Allow-Origin
+
